8d5efb7b3d0489deee3aad774458c8d913578078
Stanislaw-K <nightelf2293@gmail.com>
2024-10-16 18:32:41 +0200

    Filter module completed

---
+++ src/js/modules/filter.js
@@ -0,0 +1,96 @@
+const filter = () => {
+    const
+        menu = document.querySelector('.portfolio-menu'),
+        items = menu.querySelectorAll('li'),
+        btnAll = menu.querySelector('.all'),
+        btnLovers = menu.querySelector('.lovers'),
+        btnChef = menu.querySelector('.chef'),
+        btnGirl = menu.querySelector('.girl'),
+        btnGuy = menu.querySelector('.guy'),
+        btnGrandmother = menu.querySelector('.grandmother'),
+        btnGranddad = menu.querySelector('.granddad'),
+        wrapper = document.querySelector('.portfolio-wrapper'),
+        markAll = wrapper.querySelectorAll('.all'),
+        markGirl = wrapper.querySelectorAll('.girl'),
+        markLovers = wrapper.querySelectorAll('.lovers'),
+        markChef = wrapper.querySelectorAll('.chef'),
+        markGuy = wrapper.querySelectorAll('.guy'),
+        no = document.querySelector('.portfolio-no');
+
+    const typeFilter = (markType) => {
+        markAll.forEach(mark => {
+            mark.style.display = 'none';
+            mark.classList.remove('animated', 'fadeIn');
+        });
+
+        no.style.display = "none";
+        no.classList.remove('animated', 'fadeIn');
+
+        if (markType) {
+            markType.forEach(mark => {
+                mark.style.display = 'block';
+                mark.classList.add('animated', 'fadeIn');
+            });
+        } else {
+            no.style.display = 'block';
+            no.classList.add('animated', 'fadeIn');
+        }
+    };
+
+    // 1 var
+    // btnAll.addEventListener('click', () => {
+    //     typeFilter(markAll);
+    // });
+
+    // btnLovers.addEventListener('click', () => {
+    //     typeFilter(markLovers);
+    // });
+
+    // btnChef.addEventListener('click', () => {
+    //     typeFilter(markChef);
+    // });
+
+    // btnGuy.addEventListener('click', () => {
+    //     typeFilter(markGuy);
+    // });
+
+    // btnGirl.addEventListener('click', () => {
+    //     typeFilter(markGirl);
+    // });
+
+    // btnGrandmother.addEventListener('click', () => {
+    //     typeFilter();
+    // });
+
+    // btnGranddad.addEventListener('click', () => {
+    //     typeFilter();
+    // });
+
+    // 2 var
+    const buttons = [
+        { btn: btnAll, filter: markAll },
+        { btn: btnLovers, filter: markLovers },
+        { btn: btnChef, filter: markChef },
+        { btn: btnGuy, filter: markGuy },
+        { btn: btnGirl, filter: markGirl },
+        { btn: btnGrandmother, filter: undefined },
+        { btn: btnGranddad, filter: undefined },
+    ];
+
+    buttons.forEach(({ btn, filter }) => {
+        btn.addEventListener('click', () => typeFilter(filter));
+    });
+
+
+
+    menu.addEventListener('click', (e) => {
+        let target = e.target;
+
+        if (target && target.tagName == "LI") {
+            items.forEach(btn => btn.classList.remove('active'));
+            target.classList.add('active');
+        }
+    });
+};
+
+export default filter;
