9ac9da71ab33d7c254c9938e42c80eb4df745501
Stanislaw-K <nightelf2293@gmail.com>
2024-10-17 20:05:07 +0200

    Accordion module completed

---
+++ src/js/modules/accordion.js
@@ -0,0 +1,37 @@
+const accordion = (triggersSelector) => { // itemSelector var 1
+    const btns = document.querySelectorAll(triggersSelector);
+
+    // blocks = document.querySelectorAll(itemSelector);
+
+    // var 1
+    // blocks.forEach(block => {
+    //     block.classList.add('animated', 'fadeInDown');
+    // });
+
+    // btns.forEach(btn => {
+    //     btn.addEventListener('click', function () {
+    //         if (!this.classList.contains('active')) {
+    //             btns.forEach(btn => {
+    //                 btn.classList.remove('active', 'active-style');
+    //             });
+    //             this.classList.add('active', 'active-style');
+    //         }
+    //     });
+    // });
+
+    // var 2 (javascript)
+    btns.forEach(btn => {
+        btn.addEventListener('click', function () {
+            this.classList.toggle('active-style');
+            this.nextElementSibling.classList.toggle('active-content');
+
+            if (this.classList.contains('active-style')) {
+                this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + "px";
+            } else {
+                this.nextElementSibling.style.maxHeight = '0px';
+            }
+        });
+    });
+};
+
+export default accordion;
